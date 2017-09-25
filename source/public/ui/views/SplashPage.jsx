68130882406d77313cb4b87bddf5f52dda97c073
Jay Chang <jay.chang@momentussoftware.com>
2017-09-25 12:37:39 -0700

    Trying out new things for splash

--- source/public/ui/views/SplashPage.jsx
+++ source/public/ui/views/SplashPage.jsx
@@ -5,13 +5,36 @@ import { animateScroll } from 'react-scroll';
 import SplashMask from 'Components/SplashMask.jsx';
 
 export default class SplashPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      scale: 1/4,
+    }
+    this.handleScroll = this.handleScroll.bind(this);
+  }
+
   componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll);
     init(this.refs.typed, {
       strings:['Jay Chang :)'],
       startDelay: 400,
       backDelay: 3600,
       loop: true,
     });
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll);
+  }
+
+  handleScroll(event) {
+    let scroll = event.srcElement.body.scrollTop;
+    let maxScroll = document.querySelector('#splashpage_').offsetHeight;
+    if (scroll < maxScroll) {
+      this.setState({
+        scale: 1/4 + (scroll / (4 * maxScroll)),
+      });
+    }
   }
 
   scrollToNav() {
@@ -26,15 +49,14 @@ export default class SplashPage extends React.Component {
 
   render() {
     const styles = {
-      'backgroundColor': 'green',
+      'transform': `scale(${this.state.scale}, ${this.state.scale})`,
     }
 
     return (
-      <div id="splashpage_" style={styles} onClick={this.scrollToNav}>
+      <div id="splashpage_" onClick={this.scrollToNav}>
         <div className="splash sticky_aligner">
           <div className="sticky_content">
-            <SplashMask />
-            <div className="typed-container">
+            <div className="typed-container" style={styles}>
               <div><span className="typed-text" ref="typed" /></div>
             </div>
           </div>
