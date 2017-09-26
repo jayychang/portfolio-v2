0d537d4110e2fb391276efcd4e29c452fc8388fa
Jay Chang <jay.chang@momentussoftware.com>
2017-09-26 16:50:33 -0700

    Added loading screen while rending dom

--- source/public/App.jsx
+++ source/public/App.jsx
@@ -2,6 +2,19 @@ import React from 'react';
 import Layout from 'Layouts/Layout.jsx';
 
 export default class App extends React.Component {
+  componentDidMount() {
+    const loadingScreen = document.querySelector('#loadingScreen_')
+    if(loadingScreen) {
+      setTimeout(() => {
+        window.scrollTo(0, 0);
+        loadingScreen.classList.add('on')
+        setTimeout(() => {
+          loadingScreen.outerHTML = '';
+        }, 500);
+      }, 2000);
+    }
+  }
+
   render() {
     return (
       <Layout />
